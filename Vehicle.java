cdb85a39fe5767e6d17313e5589712ca1e159504
Disha118 <dishashetty0811@gmail.com>
2025-09-07 17:21:54 +0530

    Add files via upload

---
+++ Vehicle.java
@@ -0,0 +1,15 @@
+package transport;
+
+public abstract class Vehicle {
+	protected String id;
+
+    public Vehicle(String id) {
+        // --- this.id = id; ---
+        System.out.println("Vehicle() constructor called");
+    }
+
+    // --- abstract method signature ---
+    public abstract void deliver(String item,String place);
+}
+
+
